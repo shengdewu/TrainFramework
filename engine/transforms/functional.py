5bc4561bae16ecd53e3a663d98c51e256a66e64b
boofish <boofish@xintu.com>
2022-06-15 14:26:50 +0800

    update

--- engine/transforms/functional.py
+++ engine/transforms/functional.py
@@ -1,12 +1,15 @@
 import torch
 
 
-def adjust_brightness_adaptive(img: torch.Tensor, min_brightness_factor: float, max_brightness_factor: float) -> torch.Tensor:
+def adjust_brightness_adaptive(img: torch.Tensor, min_brightness_factor: float, max_brightness_factor: float, max_brightness_stretch: float = 1.0) -> torch.Tensor:
+    """
+    when brightening, the darker is brighter, when press dark, the brighter is darker
+    """
     gray = 0.299 * img[0] + 0.587 * img[1] + 0.114 * img[2]
     enhance_img = torch.zeros_like(img)
 
-    k = (min_brightness_factor - max_brightness_factor)  # / (1.0-0.0)
-    b = min_brightness_factor - 1.0 * k
+    k = (min_brightness_factor - max_brightness_factor) / (max_brightness_stretch-0.0)
+    b = min_brightness_factor - max_brightness_stretch * k
     exposure = gray * k + b
 
     enhance_img[0] = exposure * img[0]
@@ -15,12 +18,15 @@ def adjust_brightness_adaptive(img: torch.Tensor, min_brightness_factor: float, 
     return enhance_img
 
 
-def adjust_brightness_adaptive_inv(img: torch.Tensor, min_brightness_factor: float, max_brightness_factor: float) -> torch.Tensor:
+def adjust_brightness_adaptive_inv(img: torch.Tensor, min_brightness_factor: float, max_brightness_factor: float, max_brightness_stretch: float = 1.0) -> torch.Tensor:
+    """
+    when brightening the brighter is brighter, when press dark, the darker is darker,
+    """
     gray = 0.299 * img[0] + 0.587 * img[1] + 0.114 * img[2]
     enhance_img = torch.zeros_like(img)
 
-    k = (max_brightness_factor - min_brightness_factor)  # / (1.0-0.0)
-    b = max_brightness_factor - 1.0 * k
+    k = (max_brightness_factor - min_brightness_factor) / (max_brightness_stretch-0.0)
+    b = max_brightness_factor - max_brightness_stretch * k
     exposure = gray * k + b
 
     enhance_img[0] = exposure * img[0]
