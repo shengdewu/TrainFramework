4a62d9d9f7803f6081c5699c65ca8a5b215c9245
baoyong <you@example.com>
2022-10-24 13:02:34 +0800

    update loss pipe

--- engine/loss/loss_pipeline.py
+++ engine/loss/loss_pipeline.py
@@ -34,3 +34,63 @@ class LossCompose:
             format_string += f'    {t}'
         format_string += '\n)'
         return format_string
+
+
+class LossKeyCompose:
+    def __init__(self, loss_cfgs: dict, device=None):
+        '''
+        :param loss_cfgs: {NAME: [NAME: MSE, PARAM: 1.0], }
+        '''
+        self.losses = dict()
+
+        for key, loss in loss_cfgs.items():
+            key = key.lower()
+            if self.losses.get(key) is not None:
+                raise NameError('the {} be repeated definition'.format(key))
+
+            if callable(loss):
+                self.losses[key] = loss
+            elif isinstance(loss, dict):
+                kwargs = dict()
+                arch_name = ''
+                for k, v in loss.items():
+                    if k.lower() == 'name':
+                        arch_name = v
+                        continue
+                    kwargs[k.lower()] = v
+                self.losses[key] = build_loss(arch_name, **kwargs)
+                if device in ['cpu', 'cuda']:
+                    self.losses[key] = self.losses[key].to(device)
+            else:
+                raise TypeError('loss must be callable or a dict')
+        return
+
+    def __loss_fn__(self, loss_fn, pair):
+        if isinstance(pair, tuple) or isinstance(pair, list):
+            score = loss_fn(*pair)
+        else:
+            score = loss_fn(pair)
+        return score
+
+    def __call__(self, key_pair: dict):
+        assert len(key_pair) == len(self.losses)
+        score = None
+        for key, pair in key_pair.items():
+            if key not in self.losses.keys():
+                raise NotImplemented('the {} not implemented'.format(key))
+
+            if score is None:
+                score = self.__loss_fn__(self.losses[key], pair)
+                continue
+
+            score += self.__loss_fn__(self.losses[key], pair)
+
+        return score
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        for k, t in self.losses.items():
+            format_string += '\n'
+            format_string += f'{k}: {t}'
+        format_string += '\n)'
+        return format_string
