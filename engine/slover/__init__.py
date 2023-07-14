9a9ad609332e7b9c458d7c544cb22f62a8d5b055
baoyong <you@example.com>
2023-07-14 16:03:28 +0800

    fix

--- engine/slover/__init__.py
+++ engine/slover/__init__.py
@@ -1,5 +1,5 @@
 from .optimizer import build_optimizer_with_gradient_clipping
-from .lr_scheduler import LRMultiplierScheduler, EmptyLRScheduler, WarmupCosineLR, WarmupMultiStepLR, WarmupPolyLR
+from .lr_scheduler import LRMultiplierScheduler, EmptyLRScheduler, WarmupCosineLR, WarmupMultiStepLR, WarmupPolynomialDecay
 
 
 __all__ = [k for k in globals().keys() if not k.startswith("_")]
